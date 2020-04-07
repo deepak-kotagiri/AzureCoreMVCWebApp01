ed501abd8adc9367a43acd1cdf56b7d7027f12d1
JDSPORTS\deepak.kotagiri <d33p@k169>
2020-04-07 12:05:38 +0100

    Image Upload

---
+++ AzureCoreWebMVC/Controllers/ImagesController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AzureCoreWebMVC.Models;
+using AzureCoreWebMVC.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureCoreWebMVC.Controllers
+{
+    public class ImagesController : Controller
+    {
+        private readonly ImageStore imageStore;
+
+        public ImagesController(ImageStore imageStore)
+        {
+            this.imageStore = imageStore;
+        }
+        // GET: Images
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upload(IFormFile image)
+        {
+            if (image != null)
+            {
+                using (var stream = image.OpenReadStream())
+                {
+                    var imageId = await imageStore.SaveImage(stream);
+                    return RedirectToAction("Show", new { imageId });
+                }
+            }
+            return View();
+        }
+
+        [HttpGet("{imageId}")]
+        public ActionResult Show(string imageId)
+        {
+            var model = new ShowModel { Uri = imageStore.UriFor(imageId) };
+            return View(model);
+        }
+        // GET: Images/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Images/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Images/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Images/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Images/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Images/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Images/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ AzureCoreWebMVC/Services/ImageStore.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AzureCoreWebMVC.Services
+{
+    public class ImageStore
+    {
+
+        CloudBlobClient blobClient;
+        string baseUri = "https://deepakazurestorage01.blob.core.windows.net/";
+
+        public ImageStore()
+        {
+            var credentials = new StorageCredentials("deepakazurestorage01", "xaMOQbGWdI8iWZixJViO6yhNwzd+98uKI+Vye1CHUMR5YKEgFHMvuZ0K6USWNS7ANJTk0jo1Bl+UR1G3HEIzAQ==");
+            blobClient = new CloudBlobClient(new Uri(baseUri), credentials);
+        }
+
+        public async Task<string> SaveImage(Stream imageStream)
+        {
+            var imageId = Guid.NewGuid().ToString();
+            var container = blobClient.GetContainerReference("image");
+            var blob = container.GetBlockBlobReference(imageId);
+            await blob.UploadFromStreamAsync(imageStream);
+            return imageId;
+        }
+
+        public string UriFor(string imageId)
+        {
+            var sasPolicy = new SharedAccessBlobPolicy
+            {
+                Permissions = SharedAccessBlobPermissions.Read,
+                SharedAccessStartTime = DateTime.UtcNow.AddMinutes(-15),
+                SharedAccessExpiryTime = DateTime.UtcNow.AddMinutes(15)
+            };
+
+            var container = blobClient.GetContainerReference("image");
+            var blob = container.GetBlockBlobReference(imageId);
+            var sas = blob.GetSharedAccessSignature(sasPolicy);
+            return $"{baseUri}image/{imageId}{sas}";
+        }
+
+
+    }
+}
